86e92904f976d0b141ca8fde7fd7f315bee97573
Seth Pellegrino <seth@codecopse.net>
2022-10-10 08:56:58 -0700

    wip(sorted): 05-match-expr

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,7 +1,7 @@
 use proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote::ToTokens;
-use syn::{parse_macro_input, Item, Variant};
+use syn::{parse_macro_input, visit_mut::VisitMut, Attribute, Ident, Item, ItemFn, Pat, Variant};
 
 #[proc_macro_attribute]
 pub fn sorted(_args: TokenStream, input: TokenStream) -> TokenStream {
@@ -15,16 +15,16 @@ pub fn sorted(_args: TokenStream, input: TokenStream) -> TokenStream {
 }
 
 fn handle_item(item: syn::Item) -> Result<(), syn::Error> {
-    match item {
-        Item::Enum(e) => handle_enum(e),
-        _ => Err(syn::Error::new(
-            Span::call_site(),
-            "expected enum or match expression",
-        )),
-    }
-}
+    let e = match item {
+        Item::Enum(e) => e,
+        _ => {
+            return Err(syn::Error::new(
+                Span::call_site(),
+                "expected enum or match expression",
+            ))
+        }
+    };
 
-fn handle_enum(e: syn::ItemEnum) -> Result<(), syn::Error> {
     let mut last = match e.variants.first() {
         Some(v) => &v.ident,
         None => return Ok(()), // empty enums are sorted
@@ -38,10 +38,71 @@ fn handle_enum(e: syn::ItemEnum) -> Result<(), syn::Error> {
         let before = &e
             .variants
             .iter()
-            .filter(|v| ident < &v.ident)
-            .next()
+            .find(|v| ident < &v.ident)
             .unwrap() // ident must be < at least "last", but may come sooner
             .ident;
+        return Err(syn::Error::new_spanned(
+            ident,
+            format!("{} should sort before {}", ident, before),
+        ));
+    }
+
+    Ok(())
+}
+
+#[proc_macro_attribute]
+pub fn check(_args: TokenStream, input: TokenStream) -> TokenStream {
+    let mut item_fn = parse_macro_input!(input as ItemFn);
+
+    let mut visitor = Sorted::default();
+    visitor.visit_item_fn_mut(&mut item_fn);
+
+    let mut out = item_fn.to_token_stream();
+    out.extend(visitor.0.iter().map(syn::Error::to_compile_error));
+    out.into()
+}
+
+#[derive(Default)]
+struct Sorted(Vec<syn::Error>);
+
+impl syn::visit_mut::VisitMut for Sorted {
+    fn visit_expr_match_mut(&mut self, mexpr: &mut syn::ExprMatch) {
+        if let Some(idx) = mexpr
+            .attrs
+            .iter()
+            .position(|&Attribute { ref path, .. }| path.is_ident("sorted"))
+        {
+            mexpr.attrs.remove(idx);
+
+            self.0.extend(handle_match_expr(mexpr).err());
+        }
+    }
+}
+
+fn handle_match_expr(mexpr: &syn::ExprMatch) -> Result<(), syn::Error> {
+    fn pat_ident(pat: &Pat) -> Result<&Ident, syn::Error> {
+        match pat {
+            Pat::Ident(i) => Ok(&i.ident),
+            Pat::TupleStruct(ts) => ts.path.get_ident().ok_or_else(|| todo!()),
+            _ => todo!("{:?}", pat),
+        }
+    }
+
+    let arms = mexpr.arms.as_slice();
+    let (first, rest) = match arms {
+        &[] | &[_] => return Ok(()),
+        &[ref first, ref rest @ ..] => (pat_ident(&first.pat)?, rest),
+    };
+
+    let mut seen = vec![first];
+    for arm in rest {
+        let ident = pat_ident(&arm.pat)?;
+        if seen.last().unwrap() < &ident {
+            seen.push(ident);
+            continue;
+        }
+
+        let before = seen.iter().find(|e| ident < e).unwrap();
         return Err(syn::Error::new_spanned(
             ident,
             format!("{} should sort before {}", ident, before),
