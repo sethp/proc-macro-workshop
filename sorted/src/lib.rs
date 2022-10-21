b6fbaf7fc4b4f30ab71b3dc75325bd8d806cef21
Seth Pellegrino <seth@codecopse.net>
2022-10-20 20:10:46 -0700

    wip(sorted): 06-pattern-path

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,7 +1,10 @@
 use proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote::ToTokens;
-use syn::{parse_macro_input, visit_mut::VisitMut, Attribute, Ident, Item, ItemFn, Pat, Variant};
+use syn::{
+    parse_macro_input, visit_mut::VisitMut, Attribute, Item, ItemFn, Pat, PathArguments,
+    PathSegment, Variant,
+};
 
 #[proc_macro_attribute]
 pub fn sorted(_args: TokenStream, input: TokenStream) -> TokenStream {
@@ -80,10 +83,9 @@ impl syn::visit_mut::VisitMut for Sorted {
 }
 
 fn handle_match_expr(mexpr: &syn::ExprMatch) -> Result<(), syn::Error> {
-    fn pat_ident(pat: &Pat) -> Result<&Ident, syn::Error> {
+    fn pat_path(pat: &Pat) -> Result<Ordered, syn::Error> {
         match pat {
-            Pat::Ident(i) => Ok(&i.ident),
-            Pat::TupleStruct(ts) => ts.path.get_ident().ok_or_else(|| todo!()),
+            Pat::TupleStruct(ts) => Ok(Ordered(ts.path.clone())),
             _ => todo!("{:?}", pat),
         }
     }
@@ -91,23 +93,145 @@ fn handle_match_expr(mexpr: &syn::ExprMatch) -> Result<(), syn::Error> {
     let arms = mexpr.arms.as_slice();
     let (first, rest) = match arms {
         &[] | &[_] => return Ok(()),
-        &[ref first, ref rest @ ..] => (pat_ident(&first.pat)?, rest),
+        &[ref first, ref rest @ ..] => (pat_path(&first.pat)?, rest),
     };
 
     let mut seen = vec![first];
     for arm in rest {
-        let ident = pat_ident(&arm.pat)?;
+        let ident = pat_path(&arm.pat)?;
         if seen.last().unwrap() < &ident {
             seen.push(ident);
             continue;
         }
 
-        let before = seen.iter().find(|e| ident < e).unwrap();
+        let before = seen.iter().find(|&e| &ident < e).unwrap();
         return Err(syn::Error::new_spanned(
-            ident,
+            &ident.0,
             format!("{} should sort before {}", ident, before),
         ));
     }
 
     Ok(())
+}
+
+#[derive(Debug)]
+struct Ordered(syn::Path);
+
+impl std::fmt::Display for Ordered {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}{}",
+            if let Some(_) = self.0.leading_colon {
+                "::"
+            } else {
+                ""
+            },
+            self.0
+                .segments
+                .iter()
+                .map(|s| match s.arguments {
+                    PathArguments::None => s.ident.to_string(),
+                    _ => unimplemented!(),
+                })
+                .collect::<Vec<String>>()
+                .join("::")
+        )
+    }
+}
+
+impl Ord for Ordered {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        use std::cmp::Ordering;
+
+        match (self.0.leading_colon, other.0.leading_colon) {
+            (Some(_), None) => return Ordering::Less,
+            (None, Some(_)) => return Ordering::Greater,
+            (Some(_), Some(_)) | (None, None) => (),
+        };
+
+        let mut ss = self.0.segments.iter();
+        let mut os = other.0.segments.iter();
+
+        loop {
+            match (ss.next(), os.next()) {
+                (
+                    Some(PathSegment {
+                        ident: s,
+                        arguments: PathArguments::None,
+                    }),
+                    Some(PathSegment {
+                        ident: o,
+                        arguments: PathArguments::None,
+                    }),
+                ) => {
+                    if s < o {
+                        return Ordering::Less;
+                    } else if s > o {
+                        return Ordering::Greater;
+                    }
+                }
+                (Some(_), None) => return Ordering::Greater,
+                (None, Some(_)) => return Ordering::Less,
+                (None, None) => break,
+                _ => unimplemented!(),
+            };
+        }
+
+        Ordering::Equal
+    }
+}
+
+impl PartialOrd for Ordered {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Ordered {
+    fn eq(&self, other: &Self) -> bool {
+        self.cmp(other).is_eq()
+    }
+}
+
+impl Eq for Ordered {}
+
+#[cfg(test)]
+pub mod test {
+    use syn::parse_quote;
+
+    use crate::Ordered;
+
+    #[test]
+    fn path_ordering() {
+        // might be nice to quickcheck this
+        let mut all = [
+            parse_quote!(a),
+            parse_quote!(a::a),
+            parse_quote!(a::b),
+            parse_quote!(b::a),
+            parse_quote!(::a),
+            parse_quote!(::a::b),
+            parse_quote!(b),
+        ]
+        .map(Ordered);
+
+        all.sort();
+
+        assert_eq!(
+            [
+                // comments keep these rustfmt'd on separate lines
+                "::a",    //
+                "::a::b", //
+                "a",      //
+                "a::a",   //
+                "a::b",   //
+                "b",      //
+                "b::a",   //
+            ],
+            all.map(|o| format!("{}", o))
+        );
+    }
+
+    // fn path_to_string(p: Path) -> String {}
 }
